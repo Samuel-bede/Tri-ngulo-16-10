895fc65b93dc6ee64d92d268cf2ea54e301ee1ff
Natallyreis <130668435+Natallyreis@users.noreply.github.com>
2023-10-16 11:24:29 -0300

    Segunda versão do problema do triângulo.

--- triangulo.py
+++ triangulo.py
@@ -1,8 +1,12 @@
-#Descubra se os lados de um polígono formam triângulo.
-a = int(input('Digite um tamanho para o triângulo: '))
-b = int(input('Digite um tamanho para o triângulo: '))
-c = int(input('Digite um tamanho para o triângulo: '))
-if a == b and a == c:
-    print('Pelos dados inseridos, este polígono forma um triângulo!')
+# Validador de Triângulo
+print('\n\t\t\t --Validador de Triângulo--\n\n')
+
+a = float(input('Informe o tamanho do lado a: '))
+b = float(input('Informe o tamanho do lado b: '))
+c = float(input('Informe o tamanho do lado c: '))
+
+# Processamento e Saída
+if (a > b + c) or (b > a + c) or (c > a + b):
+    print('Pelos dados inseridos, este polígono NÃO forma um triângulo!')
 else:
-    print('Pelos dados inseridos, este polígono não forma um triângulo!')
+    print('Pelos dados inseridos, este polígono forma um triângulo!')
